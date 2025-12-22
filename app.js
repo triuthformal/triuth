1dd0b3937da73a692979a03d7d953ff29341808d
triuthformal <triuth.formal@gmail.com>
2025-12-21 23:04:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,13 +11,39 @@ async function init() {
 
 btn.addEventListener("click", async () => {
   if (!pyodide) return;
+
   out.textContent += "\nRunning main.py...\n";
 
   const code = await (await fetch("./main.py?ts=" + Date.now(), { cache: "no-store" })).text();
-  // const code = await (await fetch("./main.py")).text();
-  const result = await pyodide.runPythonAsync(code);
 
-  if (result !== undefined) out.textContent += String(result) + "\n";
+  const indented = code.split("\n").map(line => "    " + line).join("\n");
+
+  const wrapped = `
+import sys, io
+_buf = io.StringIO()
+_old = sys.stdout
+sys.stdout = _buf
+try:
+${indented}
+finally:
+    sys.stdout = _old
+_buf.getvalue()
+`;
+
+  const printed = await pyodide.runPythonAsync(wrapped);
+  if (printed) out.textContent += printed;
 });
+
+
+// btn.addEventListener("click", async () => {
+//   if (!pyodide) return;
+//   out.textContent += "\nRunning main.py...\n";
+
+//   const code = await (await fetch("./main.py?ts=" + Date.now(), { cache: "no-store" })).text();
+//   // const code = await (await fetch("./main.py")).text();
+//   const result = await pyodide.runPythonAsync(code);
+
+//   if (result !== undefined) out.textContent += String(result) + "\n";
+// });
 
 init();
